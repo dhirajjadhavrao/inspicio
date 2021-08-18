47fbe0412a9334e3db9a41723d8474372d585194
unknown <dhirajjadhavrao@gmail.com>
2021-08-18 11:17:10 +0530

    INS-1 : Initial commit

---
+++ src/main/java/com/inspicio/InspicioApplication.java
@@ -0,0 +1,13 @@
+package com.inspicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspicioApplication.class, args);
+	}
+
+}
